09e3b3a963e90e2551d241c02a25b3ba3335cdd6
Nicolas Graziano <nicolas.graziano@gmail.com>
2025-04-24 18:05:41 +0200

    some cleanup

--- src/radio1276FSK.h
+++ src/radio1276FSK.h
@@ -2,28 +2,30 @@
 #define radio1276FSK_h
 
 #include "hal/hal_io.h"
-#include <stdint.h>
 #include <array>
+#include <stdint.h>
 
 constexpr uint8_t IZAR_LENGH_3OUTOF6 = 0x45;
 constexpr uint8_t IZAR_LENGH = 30;
 
 class RadioSx1276FSK final {
 public:
-  explicit RadioSx1276FSK(lmic_pinmap const &pins, const std::array<uint8_t, 6> &meter_id);
+  explicit RadioSx1276FSK(lmic_pinmap const &pins,
+                          const std::array<uint8_t, 6> &meter_id);
   bool listen_wmbus(std::array<uint8_t, 7> &result);
   void stop_listen();
+
 private:
   void init();
   void handle_payload_ready();
   void handle_fifo_level();
 
   const std::array<uint8_t, 6> &meter_id;
-  bool listening = false;
-  std::array<uint8_t,IZAR_LENGH_3OUTOF6> buffer_raw;
-  uint8_t current_raw_byte = 0;
-  std::array<uint8_t,IZAR_LENGH> buffer;
   HalIo hal;
+  bool listening = false;
+  std::array<uint8_t, IZAR_LENGH_3OUTOF6> buffer_raw = {0};
+  uint8_t current_raw_byte = 0;
+  std::array<uint8_t, IZAR_LENGH> buffer = {0};
 
   OsTime debugtime;
 };
